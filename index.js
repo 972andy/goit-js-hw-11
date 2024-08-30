c4ec3b985e4a29151f26ee89862e3d00c359679a
972andy <sandriy972@gmail.com>
2024-08-30 11:45:27 +0000

    Deploying to gh-pages from @ 972andy/goit-js-hw-11@afb39265f55f64fe9d142ef70170013f260775ed 🚀

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-/* empty css                      */(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();
+import{a as u,S as $,i as n}from"./assets/vendor-CRwkH7JT.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))d(t);new MutationObserver(t=>{for(const s of t)if(s.type==="childList")for(const o of s.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&d(o)}).observe(document,{childList:!0,subtree:!0});function r(t){const s={};return t.integrity&&(s.integrity=t.integrity),t.referrerPolicy&&(s.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?s.credentials="include":t.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function d(t){if(t.ep)return;t.ep=!0;const s=r(t);fetch(t.href,s)}})();u.defaults.baseURL="https://pixabay.com/api/";function y(a,e){const r={params:{key:"45713386-799404e4cc786512a2668b467",image_type:"photo",orientation:"horizontal",safesearch:"true",q:a,page:e,per_page:15}};return u.get("",r)}const q=document.querySelector(".gallery-list"),h=a=>{const e=a.map(({webformatURL:r,largeImageURL:d,tags:t,likes:s,views:o,comments:b,downloads:S})=>`<li class="gallery-item">
+        <a class="gallery-link" href="${d}">
+          <img class="gallery-image" src="${r}" alt="${t}">
+            <div class="gallery-image-stats">
+            <ul class="gallery-image-stats-list">
+              <li class="gallery-image-stats-item">
+                <p class="gallery-image-stats-title">Likes</p>
+                <p class="gallery-image-stats-text">${s}</p>
+              </li>
+              <li class="gallery-image-stats-item">
+                <p class="gallery-image-stats-title">Views</p>
+                <p class="gallery-image-stats-text">${o}</p>
+              </li>
+              <li class="gallery-image-stats-item">
+                <p class="gallery-image-stats-title">Comments</p>
+                <p class="gallery-image-stats-text">${b}</p>
+              </li>
+              <li class="gallery-image-stats-item">
+                <p class="gallery-image-stats-title">Downloads</p>
+                <p class="gallery-image-stats-text">${S}</p>
+              </li>
+            </ul>
+          </div>
+        </a>
+      </li>`).join("");q.insertAdjacentHTML("beforeend",e)},f=document.querySelector(".form"),L=document.querySelector(".gallery-list"),l=document.querySelector(".loader"),c=document.querySelector(".load-more"),v=new $(".gallery-list a",{captionsData:"alt",captionDelay:250,overlayOpacity:.8});let g="",i=1,p=0,m=0,x=15;const O=async a=>{if(a.preventDefault(),g=a.target.elements.inputField.value.toLowerCase().trim(),i=1,!g){n.error({title:"Error",message:"You need to type something on field!",position:"topRight"});return}l.classList.remove("hidden");try{const e=await y(g,i);if(e.data.total===0){n.error({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight"}),l.classList.add("hidden"),L.innerHTML="",f.reset();return}else h(e.data.hits),v.refresh(),l.classList.add("hidden"),c.classList.remove("hidden")}catch(e){n.error({message:`There is an Error ${e}. Try again!`,position:"topRight"})}},P=async a=>{try{i+=1,c.classList.add("hidden"),l.classList.remove("hidden");const e=await y(g,i);if(h(e.data.hits),m=e.data.totalHits,console.log(e),console.log(i),console.log(m),i*x>=m){n.error({message:"We're sorry, but you've reached the end of search results.",position:"topRight"}),c.classList.add("hidden"),l.classList.add("hidden");return}p=L.querySelector(".gallery-item").getBoundingClientRect().height,scrollBy({top:p*2,behavior:"smooth"}),l.classList.add("hidden"),c.classList.remove("hidden"),v.refresh()}catch(e){n.error({message:`There is an Error ${e}. Try again!`,position:"topRight"})}};f.addEventListener("submit",O);c.addEventListener("click",P);
 //# sourceMappingURL=index.js.map
